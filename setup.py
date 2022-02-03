50a22aa8582051483770758bba9385bffa96aa28
Gustavo S. Teixeira <gsteixei@gmail.com>
2022-02-03 12:48:35 -0300

    Make long tasks use Celery instead of a dedicated daemon

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with django-social-layer. If not, see <http://www.gnu.org/licenses/>.
+#
 
 import os
 from setuptools import find_packages, setup
