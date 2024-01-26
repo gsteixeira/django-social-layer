c6d756d5605ed5662983ec11614c3baf91b104e2
Gustavo S. Teixeira <gsteixei@gmail.com>
2024-01-26 11:12:09 -0300

    remove deprecated tasks file

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-social-layer",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     include_package_data=True,
     license="GNU License",
